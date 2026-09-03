bd0dab0402a6656ea10bd0d922f69af25a1f20d6
Mani Chandu <chandukurra55@gmail.com>
2026-09-03 18:09:33 +0530

    Added image

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 
+const cleanEnv = (val) => (typeof val === 'string' ? val.trim().replace(/^["']|["']$/g, '') : val);
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: cleanEnv(process.env.CLOUDINARY_CLOUD_NAME),
+  api_key: cleanEnv(process.env.CLOUDINARY_API_KEY),
+  api_secret: cleanEnv(process.env.CLOUDINARY_API_SECRET),
 });
 
 module.exports = cloudinary;
